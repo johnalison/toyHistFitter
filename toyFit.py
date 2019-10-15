0cf684b6f385d4fa711337253c1ce92f5746fb96
John Alison <johnda102@gmail.com>
2019-10-15 17:19:36 -0400

    first commit

---
+++ toyFit.py
@@ -0,0 +1,69 @@
+import ROOT
+rand = ROOT.TRandom3()
+ROOT.gROOT.SetBatch(True)
+
+hBkg = ROOT.TH1F("Background","backgound",100,-10,10);
+hSig = ROOT.TH1F("Signal","signal",100,-10,10);
+hSig.SetLineColor(ROOT.kRed)
+
+for i in range(10000):
+    hBkg.Fill(rand.Gaus(0,1))
+    hSig.Fill(rand.Gaus(4,1))
+
+
+def makeFakeData(bkg,sig,mu):
+    hFakeData = bkg.Clone()
+
+    hFakeData.Add(sig, mu)
+    hFakeData.SetName("FakeData")
+    hFakeData.SetTitle("FakeData")
+    
+    return hFakeData
+
+
+def sPlusB_func(x,par):    
+    nBkg = hBkg.GetBinContent(hBkg.FindBin(x[0]))
+    nSig = hSig.GetBinContent(hSig.FindBin(x[0]))
+    return nBkg+par[0]*nSig
+
+
+tf1_sPlusB_func = ROOT.TF1("tf1_sPlusB",sPlusB_func,-10,10,1)
+tf1_sPlusB_func.SetParameter(0,1)
+print tf1_sPlusB_func.Eval(5)
+
+
+
+can = ROOT.TCanvas("canSB","canSB")
+can.cd()
+hBkg.Draw()
+hSig.Draw("same")
+can.SaveAs("RawSandB.pdf")
+
+
+hFake = makeFakeData(hBkg,hSig,0.5)
+hFake.Draw()
+can.SaveAs("FakeData.pdf")
+
+
+hFake.Fit(tf1_sPlusB_func,"Q")
+print "fitted Value",tf1_sPlusB_func.GetParameter(0)
+can.SaveAs("FakeDataWithFit.pdf")
+
+
+fitResults = ROOT.TGraph(10)
+iPt = 0
+
+for i in range(0,100,10):
+    mu = float(i)/100
+    hFake = makeFakeData(hBkg,hSig,mu)
+    tf1_sPlusB_func.SetParameter(0,1)
+    hFake.Fit(tf1_sPlusB_func,"Q")
+    mu_fit = tf1_sPlusB_func.GetParameter(0)
+    iPt += 1
+    print "True mu",mu,"Fitted mu",mu_fit
+    fitResults.SetPoint(iPt,mu,mu_fit)
+
+can.cd()
+fitResults.SetMarkerSize(10)
+fitResults.Draw("APL")
+can.SaveAs("FitResults.pdf")
